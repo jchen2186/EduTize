27f434293c513d0094f3ab0620c82b80e6226ff4
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-02-20 18:32:37 -0500

    fixed render templates finally

--- app/views.py
+++ app/views.py
@@ -2,15 +2,15 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-@app.route('/add_course')
-@app.route('/add_upcoming_assignment')
 
+@app.route('/index')
 def index():
 	return render_template("/index.html")
 
+@app.route('/add_course')
 def addCourse():
 	return render_template("/add_course.html")
 
+@app.route('/add_upcoming_assignment')
 def addUpcoming():
 	return render_template("/add_upcoming_assignment.html")
