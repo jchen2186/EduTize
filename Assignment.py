4fa730f580d2024ea7bb3ce66c20e127d13d7191
dreamer786 <km3657@nyu.edu>
2016-02-20 01:19:22 -0500

    creatin assignment class

---
+++ Assignment.py
@@ -0,0 +1,13 @@
+class Assignment():
+    course = null
+    type = null
+    deadline = null
+    grade = []
+
+    def __init__(c, t, d, g):
+	self.course = c
+	self.type = t
+	self.deadline = d
+	self.g = grade
+
+
