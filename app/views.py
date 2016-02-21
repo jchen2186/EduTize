85954f02dfd9b784f0ccd597b0e6fdebafdb46f7
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-02-21 02:33:55 -0500

    added routes and methods that lead to confirmation page

--- app/views.py
+++ app/views.py
@@ -9,26 +9,47 @@ import os
 def index():
 	return render_template("/index.html")
 
-@app.route('/new-course')
+@app.route('/new-course', methods = ['GET', 'POST'])
 def addNewCourse():
-	return render_template("/new_course.html")
+	if request.method == 'POST':
+		course = request.form['course']
+		gpa = request.form['GPA']
+		pathName = "data/user001/" + course
+		if ((os.path.isdir(pathName)) == False): # if directory does not exist
+			os.mkdir(pathName) # create the directory
+		pathName += "/" course + ".txt"
+		fo = open(pathName,'w')
+		fo.write(course + "\n")
+		fo.write("\n") # skipping line because this is for actual gpa
+		fo.write(gpa + "\n") # this line is for goal gpa
+		fo.close()
+		return render_template("/new_course_confirmation.html")
+	else:
+		return render_template("/new_course.html")
 
-@app.route('/completed-course')
+@app.route('/completed-course', methods = ['GET', 'POST'])
 def addCompletedCourse():
-	return render_template("/completed_course.html")
+	if request.method == 'POST':
+		course = request.form['course']
+		gpa = request.form['GPA']
+		pathName = "data/user001/" + course
+		if ((os.path.isdir(pathName)) == False): # if directory does not exist
+			os.mkdir(pathName) # create the directory
+		pathName += "/" course + ".txt"
+		fo = open(pathName,'w')
+		fo.write(course + "\n")
+		fo.write(gpa + "\n")
+		fo.close()
+		return render_template("/completed_course_confirmation.html")
+	else:
+		return render_template("/completed_course.html")
 
-@app.route('/new-task')
+@app.route('/new-task', methods = ['GET','POST'])
 def addNewTask():
-	return render_template("/new_task.html")
-
-@app.route('/completed-task',methods = ['GET','POST'])
-def addCompletedTask():
 	if request.method == 'POST':
 		name = request.form['taskName']
 		course = request.form['course']
 		category = request.form['category']
-		grade = request.form['grade']
-		# task = Assignment(course,category,name)
 		pathName = "data/user001/" + course
 		if ((os.path.isdir(pathName)) == False): # if directory does not exist
 			os.mkdir(pathName) # create the directory
@@ -38,6 +59,27 @@ def addCompletedTask():
 		fo.write(course + "\n")
 		fo.write(category + "\n")
 		fo.close()
-		return "Task is added!"
+		return render_template("/new_task_confirmation.html") # add 
+	else:
+		return render_template("/new_task.html")
+
+@app.route('/completed-task', methods = ['GET','POST'])
+def addCompletedTask():
+	if request.method == 'POST':
+		name = request.form['taskName']
+		course = request.form['course']
+		category = request.form['category']
+		grade = request.form['grade']
+		pathName = "data/user001/" + course
+		if ((os.path.isdir(pathName)) == False): # if directory does not exist
+			os.mkdir(pathName) # create the directory
+		pathName += "/" + name + ".txt"
+		fo = open(pathName,'w')
+		fo.write(name + "\n")
+		fo.write(course + "\n")
+		fo.write(category + "\n")
+		fo.write(grade + "\n")
+		fo.close()
+		return render_template("/completed_task_confirmation.html")
 	else:
 		return render_template("/completed_task.html")
