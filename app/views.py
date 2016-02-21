76707920c66a8f654a97b5bdf14fc5074606cfe1
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-02-21 12:06:34 -0500

    index now prints stuff

--- app/views.py
+++ app/views.py
@@ -9,11 +9,11 @@ from os.path import isfile, join
 
 @app.route('/index')
 def index():
-	pathName = "data/user001"
+	pathName = "data/user001/"
 	dataFiles = os.listdir(pathName) # list of directories
 	fileData = {} # dictionary with key: [val1,val2,val3...]
 	for course in dataFiles: # course is a directory
-		pathName += directory + "/upcoming/"
+		pathName += course + "/upcoming/"
 
 		for category in os.listdir(pathName): # directory in upcoming
 			pathName += category
@@ -28,28 +28,31 @@ def index():
 
 	# prioritize based on category, then deadline, then course's gpa
 	heirarchy = ["test","lab","homework","quiz"] # category
-	pathName = "date/user001"
-	dataFiles = os.listdir(pathName) # list of directories
+	organizedByCategory = []
+	pathName = "data/user001/"
+	dataFiles = os.listdir(pathName) # list of directories in user001
 	for course in dataFiles: # course is a directory
-		pathName += directory + "/upcoming/"
+		pathName += course + "/upcoming/"
 
 		for category in os.listdir(pathName): # directory in upcoming
 			pathName += category
-			for assignment in os.listdir(pathName):
+			for assignment in os.listdir(pathName): # assignment is file name
 				f = open(pathName + "/" + assignment,"r")
-				# name = f.readline()
-				# course = f.readline()
-				# category += f.readline()
+				f.readline()
+				f.readline()
+				f.readline()
+				deadline = f.readline()
+				temp = [course, category, deadline]
+				dictionary = {assignment[:-4]: temp} # using substring of assignment file name
+				organizedByCategory.append(dictionary)
 				fileData[assignment] = [category,course]
 			pathName = "data/user001/" + course + "/" + "upcoming" + "/"
 		pathName = "data/user001"
-	# organizedByCategory = []
-	# priority1, priority2, priority3 = 0
-	# priority1 = fileData[f]
-	# for f in fileData: # f is a key
-	# 	priority1 = f
-
-
+	
+	var1 = dict(organizedByCategory[0])
+	var2 = dict(organizedByCategory[1])
+	var3 = dict(organizedByCategory[2])
+	
 	fo = open("app/templates/index.html","w")
 	fo.write("""
 	{% extends "base.html" %}
@@ -65,12 +68,13 @@ def index():
 		<tr>
 			<td class ="ranking">
 				1
-			</td>
-			<td class = "assignment">
-				<!--insert assignment name--><br>
-				<!--insert course name--><br>
-				<!--insert type --><br>
-				<!--insert due date-->
+			</td>""")
+	fo.write("""
+			<td class = "assignment"> """ + 
+				"Assignment: " + var1.keys()[0] + "<br>" + 
+				"Course: " + var1[var1.keys()[0]][0] + "<br>" + 
+				"Category: " + var1[var1.keys()[0]][1] + "<br>" +
+				"Deadline: " + var1[var1.keys()[0]][2] + """
 			</td>
 		</tr>
 
@@ -78,11 +82,11 @@ def index():
 			<td class ="ranking">
 				2
 			</td>
-			<td class = "assignment">
-				<!--insert assignment name--><br>
-				<!--insert course name--><br>
-				<!--insert type --><br>
-				<!--insert due date-->
+			<td class = "assignment"> """+ 
+				"Assignment: " + var2.keys()[0] + "<br>" + 
+				"Course: " + var2[var2.keys()[0]][0] + "<br>" + 
+				"Category: " + var2[var2.keys()[0]][1] + "<br>" +
+				"Deadline: " + var2[var2.keys()[0]][2] + """
 			</td>
 		</tr>
 
@@ -90,11 +94,11 @@ def index():
 			<td class ="ranking">
 				3
 			</td>
-			<td class = "assignment">
-				<!--insert assignment name--><br>
-				<!--insert course name--><br>
-				<!--insert type --><br>
-				<!--insert due date-->
+			<td class = "assignment"> """+ 
+				"Assignment: " + var3.keys()[0] + "<br>" + 
+				"Course: " + var3[var3.keys()[0]][0] + "<br>" + 
+				"Category: " + var3[var3.keys()[0]][1] + "<br>" +
+				"Deadline: " + var3[var3.keys()[0]][2] + """
 			</td>
 		</tr>
 
@@ -180,6 +184,7 @@ def addNewTask():
 		name = request.form['taskName']
 		course = request.form['course']
 		category = request.form['category']
+		deadline = request.form['deadline']
 		pathName = "data/user001/" + course
 		if ((os.path.isdir(pathName)) == False): # if course directory does not exist
 			os.mkdir(pathName) # create the directory
@@ -194,6 +199,7 @@ def addNewTask():
 		fo.write(name + "\n")
 		fo.write(course + "\n")
 		fo.write(category + "\n")
+		fo.write(deadline + "\n")
 		fo.close()
 
 		f1 = open("app/templates/new_task_confirmation.html", "w")
@@ -209,6 +215,7 @@ def addNewTask():
 		<h2>Task: """ + name + """</h2>
 		<h2>Course: """ + course + """</h2>
 		<h2>Type: """ + category + """</h2>
+		<h2>Deadline: """ + deadline + """</h2>
 
 		</div>
 
